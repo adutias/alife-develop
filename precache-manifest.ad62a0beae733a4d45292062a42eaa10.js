eeec47fc7a0c4594949ecf2747d9a854536a64a7
Solomon <adutias@gmail.com>
2020-08-01 15:53:41 +0300

    Updates

---
+++ precache-manifest.ad62a0beae733a4d45292062a42eaa10.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dde0031aeadd9f377825b1cf4c7e9375",
+    "url": "/alife-develop/index.html"
+  },
+  {
+    "revision": "11b11efe668fce15bdf7",
+    "url": "/alife-develop/static/css/main.779b742e.chunk.css"
+  },
+  {
+    "revision": "379448e24419c140d68f",
+    "url": "/alife-develop/static/js/2.0a951af5.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/alife-develop/static/js/2.0a951af5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "11b11efe668fce15bdf7",
+    "url": "/alife-develop/static/js/main.74eb19ae.chunk.js"
+  },
+  {
+    "revision": "cbb686e3575a557b5a55",
+    "url": "/alife-develop/static/js/runtime-main.d8a6924e.js"
+  }
+]);
